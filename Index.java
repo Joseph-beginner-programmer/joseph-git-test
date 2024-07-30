57aa23a17e021e5f4136a93b09fe8c26e5edd53d
joseph-beginner-programmer <joseptan1512@outlook.com>
2024-07-30 15:25:35 +0700

    memodifikasi file Main.java

--- Index.java
+++ Index.java
@@ -2,5 +2,8 @@ public class Index {
     public static void main(String[] args) {
         System.out.println("saya index");
         System.out.println("tambahan");
+        System.out.println("tambahan lagi");
+
+        
     }
 }
